dced0dd5a94e5f83c38cc0729b6009ab01100b71
Anh Tu <sweantu@gmail.com>
2025-11-17 17:53:03 +0700

    feat: clean project

--- crypto-cloud-batch/clickhouse/init-scripts/04_create_output.sql
+++
@@ -1,7 +0,0 @@
-CREATE TABLE IF NOT EXISTS output (
-    ticker String,
-    price Float64,
-    event_time DateTime64(3, 'UTC')
-) ENGINE = MergeTree()
-PARTITION BY (ticker)
-ORDER BY (event_time);
