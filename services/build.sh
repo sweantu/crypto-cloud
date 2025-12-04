1db06e3bd98f2e24b763806bab176a7f67e5139c
Anh Tu <sweantu@gmail.com>
2025-12-04 11:04:10 +0700

    feat: set up local docker

--- services/build.sh
+++ services/build.sh
@@ -1,6 +1,10 @@
 #!/usr/bin/env bash
 set -euo pipefail
 
-docker build -f spark/Dockerfile -t custom-spark:3.5.4-java17 .
+docker build -f services/spark/Dockerfile -t custom-spark:3.5.4-java17 .
+    
+docker build -f services/hive/Dockerfile -t custom-hive:4.0.1 .
 
-docker build -f hive/Dockerfile -t custom-hive:4.0.1 .
+docker build -f services/airflow/Dockerfile -t custom-airflow:2.10.3-python3.11 .
+
+docker build -f services/flink/Dockerfile -t custom-flink:1.20.3-java11 .
